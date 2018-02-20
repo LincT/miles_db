9117caa75cbd46928f04ca8732d7ac36fe871e90
Linc <test@testserver.com>
2018-02-20 16:38:58 -0600

    finished unit tests for search function, also added search function

--- test_mileage.py
+++ test_mileage.py
@@ -4,7 +4,7 @@ import logging
 from unittest import TestCase, main
 from unittest.mock import patch
 logging.basicConfig(level=logging.DEBUG, filename='mileage.log', filemode='w')
-logger = logging.getLogger("jack_kerouac")  # on the road
+logger = logging.getLogger("jack_kerouac")
 
 
 class TestMileageDB(TestCase):
@@ -71,16 +71,6 @@ class TestMileageDB(TestCase):
         with self.assertRaises(Exception):
             mileage.addMiles('Car', '12.def')
 
-    @patch('builtins.print')
-    def test_search_parms(self, mock_print):
-        mileage.add_miles('purple car', 50)
-        self.assertIn("PURPLE CAR", str(mileage.search_entries('purple car'.upper())))
-
-    @patch('builtins.print')
-    def test_search_no_parms(self, mock_print):
-        mileage.add_miles('purple car', 50)
-        self.assertIn("PURPLE CAR", str(mileage.search_entries('')))
-
     # This is not a test method, instead, it's used by the test methods
     def compare_db_to_expected(self, expected):
 
@@ -97,6 +87,70 @@ class TestMileageDB(TestCase):
             self.assertEqual(expected[row[0]], row[1])
 
         conn.close()
+
+
+class TestMileageSearch(TestCase):
+    test_db_url = 'test_miles.db'
+
+    # The name of this method is important - the test runner will look for it
+    def setUp(self):
+        # Overwrite the mileage
+        mileage.db_url = self.test_db_url
+        # drop everything from the DB to always start with an empty database
+        conn = sqlite3.connect(self.test_db_url)
+        conn.execute('DELETE FROM miles')
+        conn.commit()
+        conn.close()
+        mileage.add_miles('purple car', 50)
+        mileage.add_miles('green car', 25)
+
+    def test_search_parms(self):
+        logger.debug("test_search_parms")
+        entries = str(mileage.search_entries('PURPLE CAR'))
+        self.assertIn("PURPLE CAR", entries)
+        # assert only one entry as this is a targeted query
+        self.assertEqual(len(mileage.search_entries('purple car'.upper())), 1)
+        # assert the value entered was stored to the vehicle
+        self.assertIn("PURPLE CAR: 50", entries)
+        logger.info(mileage.search_entries('PURPLE CAR'))
+        expected_data = {
+            "PURPLE CAR": 50,
+            "GREEN CAR": 25
+        }
+        self.compare_db_to_expected(expected_data)
+
+        # now searching with bad parameters
+        self.assertEqual(mileage.search_entries('pizza'), [])
+        self.assertEqual(mileage.search_entries('BLUE CAR'), [])
+        self.assertEqual(mileage.search_entries(0), [])
+        self.assertEqual(mileage.search_entries("\n"), [])
+
+    def test_search_no_parms(self):
+        logger.debug("test_search_no_parms")
+        entries = str(mileage.search_entries())
+        logger.info(entries)
+        expected_data = {
+            "PURPLE CAR": 50,
+            "GREEN CAR": 25
+        }
+        self.compare_db_to_expected(expected_data)
+
+    # This is not a test method, instead, it's used by the test methods
+    def compare_db_to_expected(self, expected):
+
+        conn = sqlite3.connect(self.test_db_url)
+        cursor = conn.cursor()
+        all_data = cursor.execute('SELECT * FROM MILES').fetchall()
+
+        # Same rows in DB as entries in expected dictionary
+        self.assertEqual(len(expected.keys()), len(all_data))
+
+        for row in all_data:
+            # Vehicle exists, and mileage is correct
+            self.assertIn(row[0], expected.keys())
+            self.assertEqual(expected[row[0]], row[1])
+        conn.close()
+
 
 if __name__ == '__main__':
     main()
